844d671fae0b80833f617ec57d064d06c9935a33
phaman <phaman@localhost.localdomain>
2025-08-07 20:42:49 +0700

    feat: add logUnexpectedFileLimit, logMulterError, logUnknownUploadError

--- src/config/loggerConfig.js
+++ src/config/loggerConfig.js
@@ -7,7 +7,7 @@ const logger = winston.createLogger({
     // level: 'info': tức là ghi log từ info trở lên (info, warn, error)
     // Những log nhỏ hơn (debug) sẽ bị bỏ qua nếu không khai báo
     level: 'info',
-    
+
     format: winston.format.combine(
         winston.format.printf(({ level, message, ...meta }) => {
         const timestamp = moment().tz('Asia/Ho_Chi_Minh').format('YYYY-MM-DD HH:mm:ss');
@@ -24,6 +24,41 @@ const logger = winston.createLogger({
 
 export default logger;
 
+export const logUnexpectedFileLimit = (req) => {
+  logger.warn('Vượt quá số lượng file cho phép.', {
+    ip: req.ip,
+    url: req.originalUrl,
+    method: req.method,
+    filesReceived: req.files?.length || 0,
+  });
+};
+
+export const logMulterError = (err, req) => {
+  logger.warn('Lỗi multer khác.', {
+    error: err.message,
+    code: err.code,
+    ip: req.ip,
+    url: req.originalUrl,
+  });
+};
+
+export const logUnknownUploadError = (err, req) => {
+  logger.error('Lỗi không xác định khi upload file.', {
+    error: err.message,
+    ip: req.ip,
+    url: req.originalUrl,
+  });
+};
+
+
+export const logUploadSuccess = (req) => {
+  logger.info(`Upload thành công ${req.files.length} file.`, {
+    ip: req.ip,
+    url: req.originalUrl,
+    method: req.method,
+    filenames: req.files.map(f => f.originalname),
+  });
+};
 
 
 
